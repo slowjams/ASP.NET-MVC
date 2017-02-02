5704d7d307f7eecf6832c9a3a6523c392b4c7c39
slowjams <lxdthriller@gmail.com>
2017-02-02 16:38:05 +1100

    RESTful APIs added

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //public MembershipType membershipType { get; set; }
+
+        
+        public byte membershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a movie's name.")]
+        public string Name { get; set; }
+
+        //[Display(Name = "Genre")]
+        //[Required(ErrorMessage = "Please choose an genre")]
+        public Genre Genre { get; set; }
+
+        [Required(ErrorMessage = "Please choose an genre")]
+        public byte GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        //[Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        
+        [Range(1, 20)]
+        public byte? NumberInStock { get; set; }
+    }
+}
